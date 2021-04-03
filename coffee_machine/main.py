9aacfd2739c8da1302a465aa38089cdc5e06717c
Bruno Tougeiro <b.tougeiro@icloud.com>
2021-04-03 22:59:24 +0100

    Added TODOs, document requirements and initial data.

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -1,1 +1,8 @@
-print("Coffee Machine in Development.")
+# Program requirements
+# TODO: 1. Prompt user by asking "What would you like? (espresso/latte/cappuccino):"
+# TODO: 2. Turn off the Coffee Machine by entering “off” to the prompt.
+# TODO: 3. Print report.
+# TODO: 4. Check resources sufficient?
+# TODO: 5. Process coins.
+# TODO: 6. Check transaction successful?
+# TODO: 7. Make Coffee.

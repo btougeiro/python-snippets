a388643ab7d31cd903ebdad238ae7b23dac93e8d
Bruno Tougeiro <b.tougeiro@icloud.com>
2021-04-29 02:23:27 +0100

    added us_state_game guess

---
+++ us_states_game/main.py
@@ -0,0 +1,38 @@
+import turtle
+import os
+import pandas
+
+US_STATES = 50
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = f"{os.getcwd()}\image.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < US_STATES:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+        guessed_states.append(answer_state)
+
+screen.exitonclick()

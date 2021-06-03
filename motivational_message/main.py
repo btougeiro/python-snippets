caf2f3a673f76c4aeaab597d3b9547272b2a8bdf
Bruno Tougeiro <b.tougeiro@icloud.com>
2021-06-03 04:35:48 +0100

    removed empty spaces

--- motivational_message/main.py
+++ motivational_message/main.py
@@ -14,7 +14,7 @@ if weekday in range(0, 5):
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
-    
+
     with smtp.SMTP(host=host, port=port) as connection:
         connection.starttls()
         connection.login(user=email, password=password)
